ef8c020b7df61b1ae1e1440fec5c810475d877bf
DsAndrea <48347999+DsAndrea@users.noreply.github.com>
2022-09-20 10:34:24 +0200

    Update RubricheCSV.java

--- Hopper/src/rubriche/RubricheCSV.java
+++ Hopper/src/rubriche/RubricheCSV.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Rubriche {
+public class RubricheCSV {
 
 	public static void main(String args[]){
 		

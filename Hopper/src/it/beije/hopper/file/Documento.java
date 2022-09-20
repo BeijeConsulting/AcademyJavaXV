7645efa9f876757a132d92c600c44aa5d1d3793e
RaffaeleSaponara <raffaele.saponara@beije.it>
2022-09-20 14:24:12 +0200

    esercizio parserXML

---
+++ Hopper/src/it/beije/hopper/file/Documento.java
@@ -0,0 +1,46 @@
+package it.beije.hopper.file;
+
+import java.util.ArrayList;
+
+public class Documento {
+    private String root;
+    private ArrayList<Elemento> parentThree;
+    public Documento(ArrayList<Elemento> elementi){
+        setRoot(elementi.get(0));
+        setParentThree(elementi);
+    }
+
+    public void setRoot(Elemento elemento){ this.root = elemento.getNome(); }
+    public String getRootElement(){ return this.root; }
+
+    public String[] getChildNotes(Elemento elemento){
+        String[] childs = null;
+        for (Elemento e: this.parentThree) {
+            
+        }
+        return childs;
+    }
+    //   public static String[] getChildElements(){}
+
+    //  public static String[] getElementsByTagName(){}
+
+    //   public static String[] getTagName(){}
+
+    //  public static String[] getTextContext(){}
+
+    //  public static String[] getAttributes(){}*/
+
+    public void setParentThree(ArrayList<Elemento> elementi){
+        this.parentThree = parentThree;
+    }
+    public static String indentazione(int j){
+        String result="";
+        for(int i =0; i<j; i++){
+            result += "\t";
+        }
+        return result;
+    }
+
+}
+
+

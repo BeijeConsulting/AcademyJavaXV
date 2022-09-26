a70d9ae924488de75e77dd68046da01836d32326
StefanoC96 <cavaste96@gmail.com>
2022-09-26 17:39:44 +0200

    rubrica hibernate

---
+++ Hopper/src/it/beije/hopper/rubrica/RubricaHibernate.java
@@ -0,0 +1,12 @@
+package it.beije.hopper.rubrica;
+
+import org.hibernate.Session;
+
+public class RubricaHibernate {
+
+	public static void main(String[] args) {
+		Session session = HBMsessionFactory.openSession();		
+
+	}
+
+}

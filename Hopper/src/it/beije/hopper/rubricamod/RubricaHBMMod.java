08cfd344fc7c344c2441ced582d8998b8560fef6
michaelangelovalente <miky10.mv@gmail.com>
2022-09-27 19:31:43 +0200

    Working on Update

--- Hopper/src/it/beije/hopper/rubricamod/RubricaHBMMod.java
+++ Hopper/src/it/beije/hopper/rubricamod/RubricaHBMMod.java
@@ -48,22 +48,22 @@ public class RubricaHBMMod {
 //		System.out.println(contatti.size());
 
 
-		//INSERT HQL CLI
-		String userIn = null;
-		while(true){
-			System.out.println("Continue data insertion?");
-			userIn = scanner.nextLine();
-			if(!userIn.equalsIgnoreCase("yes")) break;
-			insertToDB(session, scanner);
-		}
-
-
-		//SELECT HQL CLI
-		List<Contatto> contatti = selectFromDB(session, scanner );
-		//System.out.println(contatti);
-		for( Contatto c : contatti ){
-			System.out.println(c);
-		}
+//		//INSERT HQL CLI
+//		String userIn = null;
+//		while(true){
+//			System.out.println("Continue data insertion?");
+//			userIn = scanner.nextLine();
+//			if(!userIn.equalsIgnoreCase("yes")) break;
+//			insertToDB(session, scanner);
+//		}
+//
+//
+//		//SELECT HQL CLI
+//		List<Contatto> contatti = selectFromDB(session, scanner );
+//		//System.out.println(contatti);
+//		for( Contatto c : contatti ){
+//			System.out.println(c);
+//		}
 
 
 		//Update HQL CLI
@@ -93,89 +93,94 @@ public class RubricaHBMMod {
 	//UPDATE
 	public static void updateDB(Session session, Scanner scanner){
 		Transaction transaction = session.beginTransaction();
-		List<Contatto> contatti = readDB("SELECT c FROM contatto as c", session);
+		List<Contatto> contatti = readDB("SELECT c FROM Contatto as c", session);
 		Contatto contatto = null;
 		FROMBEG: while( true ){
 			System.out.println("Search by 'name' or 'last name'?('exit' to exit program)");
 			String usrIn = scanner.nextLine();
+			String dataIn = null;
 
-			if(scanner.nextLine().equalsIgnoreCase("exit")){
+			if(usrIn.equalsIgnoreCase("exit")){
 				break;
 			}else if( usrIn.equalsIgnoreCase("name")){
 				System.out.println("Name: ");
-				usrIn = scanner.nextLine();
+				dataIn = scanner.nextLine();
 			}else if( usrIn.equalsIgnoreCase("last name") ){
 				System.out.println("Last name: ");
-				usrIn = scanner.nextLine();
+				dataIn = scanner.nextLine();
 			}
 
 
 			for( Contatto c : contatti ){
-				if( usrIn.equalsIgnoreCase("")){
+				if( dataIn.equalsIgnoreCase("")){
 					System.out.println("Empty input..");
 					continue FROMBEG;
 				}
 				if( (usrIn.equalsIgnoreCase("name") &&
-						c.getNome().equalsIgnoreCase(usrIn)) ||
+						c.getNome().equalsIgnoreCase(dataIn)) ||
 						(usrIn.equalsIgnoreCase("last name") &&
-						c.getCognome().equalsIgnoreCase(usrIn) ) ){
+						c.getCognome().equalsIgnoreCase(dataIn) ) ){
 					contatto = c;
 					break;
 				}
 			}
 
 
-			while( true ){
-				System.out.println("You are modifying " + contatto.getNome()+ "\nContinue?(yes / no)");
-				if( scanner.nextLine().equalsIgnoreCase("yes")){
-					System.out.println("Modify 'name', 'last name', 'phone', 'email', 'notes' ?");
-					usrIn = scanner.nextLine();
 
-					//TODO improve--> too many repeating code segm...
-					if( usrIn.equalsIgnoreCase("name")){
-						usrIn = scanner.nextLine();
-						if( usrIn.length()==0 ){
-							System.out.println("Empty name field...");
-							continue;
-						}
-						contatto.setNome(usrIn);
-					}else if( usrIn.equalsIgnoreCase("last name")){
-						usrIn = scanner.nextLine();
-						if( usrIn.length()==0 ){
-							System.out.println("Empty last name field...");
-							continue;
-						}
-						contatto.setCognome(usrIn);
-					}else if( usrIn.equalsIgnoreCase("email")){
-						usrIn = scanner.nextLine();
-						if( usrIn.length()==0 ){
-							System.out.println("Empty email field...");
-							continue;
-						}
-						contatto.setEmail(usrIn);
-					}else if( usrIn.equalsIgnoreCase("notes")){
-						usrIn = scanner.nextLine(); // Fix --> multiple line input
-						if( usrIn.length()==0 ){
-							System.out.println("Empty notes field...");
-							continue;
-						}
-						contatto.setNote(usrIn);
-
-					}else{
-						System.out.println("Invalid input for modification of " + contatto.getNome() + " : " + contatto.getId());
-					}
-				}else{
-					break;
-				}
-			}
-
-			session.save(contatto);
-			System.out.println("Confirm modifications on "  + contatto.getNome() + " : " + contatto.getId() + "? ('yes' or 'no')");
+			System.out.println("You are modifying " + contatto.getNome()+ "\nContinue?(yes / no)");
 			if( scanner.nextLine().equalsIgnoreCase("yes")){
-				transaction.commit();
+				System.out.println("Modify 'name', 'last name', 'phone', 'email', 'notes' ?");
+				usrIn = scanner.nextLine();
+
+				//TODO improve--> too many repeating code segm...
+				if( usrIn.equalsIgnoreCase("name")){
+					usrIn = scanner.nextLine();
+					if( usrIn.length()==0 ){
+						System.out.println("Empty name field...");
+						continue;
+					}
+					contatto.setNome(usrIn);
+				}else if( usrIn.equalsIgnoreCase("last name")){
+					usrIn = scanner.nextLine();
+					if( usrIn.length()==0 ){
+						System.out.println("Empty last name field...");
+						continue;
+					}
+					contatto.setCognome(usrIn);
+				}else if( usrIn.equalsIgnoreCase("email")){
+					usrIn = scanner.nextLine();
+					if( usrIn.length()==0 ){
+						System.out.println("Empty email field...");
+						continue;
+					}
+					contatto.setEmail(usrIn);
+				}else if( usrIn.equalsIgnoreCase("notes")){
+					usrIn = scanner.nextLine(); // Fix --> multiple line input
+					if( usrIn.length()==0 ){
+						System.out.println("Empty notes field...");
+						continue;
+					}
+					contatto.setNote(usrIn);
+
+				}else{
+					System.out.println("Invalid input for modification of " + contatto.getNome() + " : " + contatto.getId());
+				}
+
+				session.save(contatto);
+				System.out.println("Confirm modifications on "  + contatto.getNome() + " : " + contatto.getId() + "? ('yes' or 'no')");
+				if( scanner.nextLine().equalsIgnoreCase("yes")){
+					transaction.commit();
+					System.out.println(contatto.getNome() + ":" + contatto.getId()+ "Modified.");
+				}else{
+					transaction.rollback();
+					System.out.println("Modification cancelled.");
+				}
 			}else{
-				transaction.rollback();
+				System.out.println("Modification cancelled");
 			}
+
+
+
 
 		}
 	}
@@ -220,13 +225,14 @@ public class RubricaHBMMod {
 
 
 	private static List<Contatto> readDB(String finalQuery, Session session){
-		return session.createQuery(finalQuery).getResultList();
+		Query<Contatto> query = session.createQuery(finalQuery);
+		return query.getResultList();
 	}
+
 
 	//CLI
 	//Select HQL
 	public static List<Contatto> selectFromDB(Session session, Scanner scanner){
-
 		StringBuilder partialQuery = new StringBuilder("SELECT c FROM Contatto as c");
 		String finalQuery = "";
 		System.out.println("Do you want all the table's contents? yes or no");

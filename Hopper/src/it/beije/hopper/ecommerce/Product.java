4964ff937a5503e4153bc56b802cc3317e3924e0
ivfly <iv0fly@gmail.com>
2022-10-03 16:41:02 +0200

    eCommerce

---
+++ Hopper/src/it/beije/hopper/ecommerce/Product.java
@@ -0,0 +1,108 @@
+package it.beije.hopper.ecommerce;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `desc` varchar(2000) DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `rating` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
+ */
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "desc")
+	private String desc;
+
+	@Column(name = "price")
+	private Double price;
+
+	@Column(name = "quantity")
+	private Integer quantity;
+
+	@Column(name = "rating")
+	private Integer rating;
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+	
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder()
+				.append("{ id : ").append(this.id)
+				.append(", name : ").append(this.name)
+				.append(", desc : ").append(this.desc)
+				.append(", price : ").append(this.price)
+				.append(", quantity : ").append(this.quantity)
+				.append(", rating : ").append(this.rating)
+				.append(" }");
+		
+		return builder.toString();
+	}
+}

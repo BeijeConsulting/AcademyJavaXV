2e0136ca46bbc6be0e448c03b09a229b50a6fe05
Exsplosed <34218042+Exsplosed@users.noreply.github.com>
2022-10-04 14:07:31 +0200

    Gestore Rubrica con JPA aggiornato al 04.10.2022 h 14.06

---
+++ Hopper/src/it/beije/hopper/ecommercedecataldo/Ecommerce.java
@@ -0,0 +1,29 @@
+package it.beije.hopper.ecommercedecataldo;
+
+import static it.beije.hopper.gestorerubrica.Utilities.*;
+//Bean JPA
+import it.beije.hopper.ecommerce.Item;
+import it.beije.hopper.ecommerce.Order;
+import it.beije.hopper.ecommerce.Product;
+import it.beije.hopper.ecommerce.User;
+
+//Librerie JPA
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.logging.Level;
+
+//Strutture utili per ingresso e storage dati nel programma
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Ecommerce {
+    static Scanner inputTastiera;
+    public static void main(String []args){
+        System.out.println("Benvenuto nell'ecommerce a linea di comando");
+    }
+    public static void prova(){
+
+    }
+}

9edf99c13a1d989c7aa64e3b6108bf0e5e14981f
ivfly <iv0fly@gmail.com>
2022-10-12 16:59:13 +0200

    @Repository

---
+++ HopperSpring/src/main/java/it/beije/hopper/JpaEntityManager.java
@@ -0,0 +1,30 @@
+package it.beije.hopper;
+
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JpaEntityManager {
+	
+	private static EntityManagerFactory emfactory = null;
+
+	private JpaEntityManager() {}
+	
+	@Bean(name = "entityManagerFactory")
+	public static synchronized EntityManagerFactory getInstance() {
+		try {
+			if (emfactory == null) {
+				emfactory = Persistence.createEntityManagerFactory("hopper");
+			}
+			return emfactory;
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+}

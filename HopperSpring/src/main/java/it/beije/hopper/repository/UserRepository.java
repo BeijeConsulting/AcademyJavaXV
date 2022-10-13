2925941cbf7d50fdec65caea367fa2b6ec5f7c40
ivfly <iv0fly@gmail.com>
2022-10-13 15:06:08 +0200

    @Query + @OneToMany

--- HopperSpring/src/main/java/it/beije/hopper/repository/UserRepository.java
+++ HopperSpring/src/main/java/it/beije/hopper/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package it.beije.hopper.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import it.beije.hopper.model.User;
@@ -16,4 +19,14 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	public abstract User findByEmailAndPassword(String email, String password);
 
 	public abstract List<User> findByLastName(String lastName);
+	
+	//@Query(value = "SELECT u FROM User u WHERE firstName = :name")//JPA
+	@Query(nativeQuery = true, value = "SELECT * FROM users WHERE name = :name")//JDBC
+	public abstract List<User> loadByName(@Param("name") String firstName);
+	
+	//@Query(nativeQuery = true, value = "SELECT * FROM users u JOIN orders o ON o.user_id = u.id WHERE o.datetime > :date GROUP BY u.id")
+	@Query(nativeQuery = true, value = "SELECT * FROM orders o JOIN users u ON o.user_id = u.id WHERE o.datetime > :date GROUP BY u.id")
+	public abstract List<User> loadLastClients(@Param("date") LocalDate date);
+	
+	
 }

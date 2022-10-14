5a0fa964344c682ed27bb31de2b53029e98d3371
manuelx97 <80793548+manuelx97@users.noreply.github.com>
2022-10-14 11:09:36 +0200

    Add Packages

--- BeijeMusicStore/src/main/java/it/beije/musicstore/ConfigurationClass.java
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/ConfigurationClass.java
@@ -1,6 +1,25 @@
 package it.beije.musicstore;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(value = {"it.beije.musicstore.repository"})
 public class ConfigurationClass {
 	//organizzate bene i package...
+	
+	@Primary
+    @Bean(name="transactionManager")
+    public PlatformTransactionManager dbTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(JpaEntityManager.getInstance());
+        return transactionManager;
+    }
 }

d8a0800136429f818dd47740dfd2b4550642d5cf
Alessandro Musto <112927491+Alessandro-Musto@users.noreply.github.com>
2022-10-14 12:19:42 +0200

    Model Artista e Canzone

--- BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artista.java
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artista.java
@@ -1,4 +1,51 @@
 package it.beije.musicstore.model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Artista {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "genere")
+    private Integer genere;
+
+    //Getter e setter
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getGenere() {
+        return genere;
+    }
+
+    public void setGenere(Integer genere) {
+        this.genere = genere;
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder()
+                .append("{id: ").append(this.id)
+                .append(",nome: ").append(this.nome)
+                .append(",genere: ").append(this.genere);
+        return builder.toString();
+    }
 }

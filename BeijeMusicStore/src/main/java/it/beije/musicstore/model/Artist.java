3899438c21bcd08e0e89aa986ef2e8c352b4d399
manuelx97 <80793548+manuelx97@users.noreply.github.com>
2022-10-14 10:13:23 +0200

    Start

---
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artist.java
@@ -0,0 +1,19 @@
+package it.beije.musicstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "artist")
+public class Artist {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+}

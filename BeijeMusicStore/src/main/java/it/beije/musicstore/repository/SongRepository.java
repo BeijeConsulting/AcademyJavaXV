634a288e2f5244fe617efd330186ea7683cd5dd5
manuelx97 <80793548+manuelx97@users.noreply.github.com>
2022-10-14 12:53:42 +0200

    Start v2

--- BeijeMusicStore/src/main/java/it/beije/musicstore/repository/SongRepository.java
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/repository/SongRepository.java
@@ -1,13 +1,13 @@
-//package it.beije.musicstore.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.stereotype.Repository;
-//
-//import it.beije.musicstore.model.Song;
-//
-//@Repository
-//public interface SongRepository extends JpaRepository<Song, Integer>{
-//
-//}
+package it.beije.musicstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import it.beije.musicstore.model.Song;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Integer>{
+
+}

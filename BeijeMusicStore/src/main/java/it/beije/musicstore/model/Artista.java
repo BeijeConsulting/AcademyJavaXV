d633cba343cc9bdc61901feaa8f52a14f45f9af5
StefanoC96 <cavaste96@gmail.com>
2022-10-14 12:17:48 +0200

    beijemusic

--- BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artista.java
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artista.java
@@ -1,10 +1,14 @@
 package it.beije.musicstore.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="artista")
 public class Artista {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- BeijeMusicStore/src/main/java/it/beije/musicstore/model/Canzone.java
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Canzone.java
@@ -1,11 +1,16 @@
 package it.beije.musicstore.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="canzone")
 public class Canzone {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

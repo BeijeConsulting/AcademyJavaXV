85e10621b1338dc01ad87965c702767eeacfc7cb
Exsplosed <34218042+Exsplosed@users.noreply.github.com>
2022-10-14 11:08:03 +0200

    Configurazione completa per MusicBeije

---
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Album.java
@@ -0,0 +1,88 @@
+package it.beije.musicstore.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="album")
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="id_artista")
+    private Integer idArtista;
+
+    @Column(name="titolo")
+    private String titolo;
+
+    @Column(name="data_di_uscita")
+    private LocalDateTime data;
+
+    @Column(name="genere")
+    private String genere;
+
+    @Column(name="n_canzoni")
+    private Integer nCanzoni;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdArtista() {
+        return idArtista;
+    }
+
+    public void setIdArtista(Integer idArtista) {
+        this.idArtista = idArtista;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    public Integer getnCanzoni() {
+        return nCanzoni;
+    }
+
+    public void setnCanzoni(Integer nCanzoni) {
+        this.nCanzoni = nCanzoni;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "id=" + id +
+                ", idArtista=" + idArtista +
+                ", titolo='" + titolo + '\'' +
+                ", data=" + data +
+                ", genere='" + genere + '\'' +
+                ", nCanzoni=" + nCanzoni +
+                '}';
+    }
+}
---
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Canzone.java
@@ -0,0 +1,89 @@
+package it.beije.musicstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+public class Canzone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="id_artista")
+    private Integer idArtista;
+
+    @Column(name="id_album")
+    private Integer idAlbum;
+
+    @Column(name="titolo")
+    private String titolo;
+
+    @Column(name="data_uscita")
+    private LocalDateTime data;
+
+    @Column(name="genere")
+    private String genere;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdArtista() {
+        return idArtista;
+    }
+
+    public void setIdArtista(Integer idArtista) {
+        this.idArtista = idArtista;
+    }
+
+    public Integer getIdAlbum() {
+        return idAlbum;
+    }
+
+    public void setIdAlbum(Integer idAlbum) {
+        this.idAlbum = idAlbum;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    @Override
+    public String toString() {
+        return "Canzone{" +
+                "id=" + id +
+                ", idArtista=" + idArtista +
+                ", idAlbum=" + idAlbum +
+                ", titolo='" + titolo + '\'' +
+                ", data=" + data +
+                ", genere='" + genere + '\'' +
+                '}';
+    }
+}

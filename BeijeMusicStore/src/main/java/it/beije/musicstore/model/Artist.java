1a317a9adb132bedc17ff1b62c5c88c5f59c3bbe
manuelx97 <80793548+manuelx97@users.noreply.github.com>
2022-10-14 10:24:31 +0200

    add Model

--- BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artist.java
+++ BeijeMusicStore/src/main/java/it/beije/musicstore/model/Artist.java
@@ -8,12 +8,46 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "artist")
+@Table(name = "artista")
 public class Artist {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "genre")
+	private String genre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
 
 }

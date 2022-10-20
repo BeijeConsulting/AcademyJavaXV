5aa2fe05526017d595d3ead43ce17cb3b2b0bf25
StefanoC96 <cavaste96@gmail.com>
2022-10-20 15:19:14 +0200

    getbyid

--- Magazzino/src/main/java/it/beije/magazzino/controller/ProdottoController.java
+++ Magazzino/src/main/java/it/beije/magazzino/controller/ProdottoController.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import it.beije.magazzino.model.Prodotto;
 import it.beije.magazzino.service.ProdottoService;
 
@@ -34,5 +37,34 @@ public class ProdottoController {
 		
 		return "lista_prodotti";
 	}
+	
+	
+	@RequestMapping(value = "/formprodotti", method = RequestMethod.GET)
+	public String form() {
+		System.out.println("GET login...");
 
+		return "form_id";
+	}
+	
+	
+	@RequestMapping(value = "/formprodotti", method = RequestMethod.POST)
+	public String form(HttpSession session,
+			@RequestParam(name = "id", required = false) Integer id,
+			Model model) {
+		
+		System.out.println("POST id...");
+		
+		System.out.println("id : " + id);
+		
+		if (id != null) {
+			Prodotto prodotto = prodottoService.findById(id);
+				
+				model.addAttribute("prodotto", prodotto);
+								
+				return "prodotto_id";
+			} 
+
+		return "form_id";
+	}		
+	
 }
---
+++ Magazzino/src/main/webapp/WEB-INF/views/prodotto_id.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>PRODOTTO CON ID</title>
+</head>
+<body>
+
+
+
+<br>
+${prodotto}
+</body>
+</html>
